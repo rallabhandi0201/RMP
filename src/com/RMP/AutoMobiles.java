3b0f79b739d00fe34220a83f8ca3a2e92d58be3f
Sairam <rallabhandi0201@gmail.com>
2022-04-10 20:45:02 +0530

    created AutoMobiles and Commodities classes

---
+++ src/com/RMP/AutoMobiles.java
@@ -0,0 +1,13 @@
+package com.RMP;
+
+public class AutoMobiles {
+    int NoOfVehicle;
+    String VehicleNumber,type,VehicleName;
+
+    public AutoMobiles(int noOfVehicle, String vehicleNumber, String type, String vehicleName) {
+        NoOfVehicle = noOfVehicle;
+        VehicleNumber = vehicleNumber;
+        this.type = type;
+        VehicleName = vehicleName;
+    }
+}
---
+++ src/com/RMP/Commodities.java
@@ -0,0 +1,11 @@
+package com.RMP;
+
+public class Commodities {
+    int NoOfCommodities;
+    String Type;
+
+    public Commodities(int noOfCommodities, String type) {
+        NoOfCommodities = noOfCommodities;
+        Type = type;
+    }
+}
