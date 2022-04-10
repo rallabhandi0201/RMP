1014c794da60ae4086aca115e6ff48ed439fc5f5
N-Harsha <harsha0770@gmail.com>
2022-04-10 20:41:58 +0530

    Tenant class created.

--- src/com/RMP/Tenant.java
+++ src/com/RMP/Tenant.java
@@ -1,5 +1,20 @@
 package com.RMP;
+import java.util.*;
+
 
 public class Tenant {
+    int T_id;
+    List<Transactions> lt=new ArrayList<>();
+    List<AutoMobiles> lv=new ArrayList<>();
+    List<Commodities> lc=new ArrayList<>();
+    String name,guardian_name,ph,aph;//ph phone number, aph alternate phone number
+    String job_id;//job id for tenant
+    String adhar_no;
+    String Perm_address;
+    Date due_date;
+    Date doj;//date of joining
+    int occupants;//no.of people staying
+    int amount;
+    int adv_mon;
 
 }
